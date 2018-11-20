719f4554cfa93c2b7f71814277b11753e57286f9
Stefan Rimola <stefanrimola@gmail.com>
2018-11-20 13:26:19 -0800

    CalendarView skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CalendarView from './components/CalendarView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <CalendarView />
       </div>
     );
   }
