5f1fe1bb24f80f9f582b3ac4d86e2723a97f981f
Stefan Rimola <stefanrimola@gmail.com>
2018-11-20 13:37:01 -0800

    CalendarView placeholder

---
+++ src/components/CalendarView/CalendarView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CalendarView.css';
+
+export default function CalendarView() {
+  return (
+    <div className='calendarView'>
+      CalendarView
+    </div>
+  );
+}
