02164814b0510913e47cb618ae94ca89c9672337
Stefan Rimola <stefanrimola@gmail.com>
2018-11-20 14:02:41 -0800

    Button proxy component

--- src/components/HeaderButton/HeaderButton.js
+++ src/components/HeaderButton/HeaderButton.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './HeaderButton.css';
+import Button from '../Button/Button';
 import AddButton from '../Buttons/AddButton';
 import SettingsButton from '../Buttons/SettingsButton';
 
 class HeaderButton extends Component {
   render() {
     return (
-      <a className="headerButton" href={this.props.type}>
+      <Button className="headerButton" onClick={this.props.type}>
         {this.props.type === 'settings' ?
         <SettingsButton /> :
         <AddButton />}
-      </a>
+      </Button>
     );
   }
 }
