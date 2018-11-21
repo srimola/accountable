8aba1b5578f53457d43f0111827b7f7d6b4fdf99
Stefan Rimola <stefanrimola@gmail.com>
2018-11-20 16:24:40 -0800

    Initial Firebase/Redux setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import CalendarView from './components/CalendarView';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <CalendarView />
+        <TodoList />
       </div>
     );
   }
